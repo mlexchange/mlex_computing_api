eec636bef4639d09a05daa4b267a4608a7e3af4b
Tanny Chavez Esparza <tanchavez1@gmail.com>
2022-06-13 13:02:23 -0700

    new pop-up msgs and delete database option

--- front_end/frontend.py
+++ front_end/frontend.py
@@ -170,6 +170,24 @@ RESOURCES_TABLE = dbc.Card(
             dbc.CardHeader("List of Computing Resources"),
             dbc.CardBody(
                 children=[
+                    dbc.Row(
+                        [dbc.Col(dbc.Button("RESET DATABASE",
+                                       outline=True,
+                                       color="danger",
+                                       id="reset-database",
+                                       style={"text-transform": "none", "width":"100%"})),
+                         dbc.Col(dbc.Button("RESET HOST",
+                                       outline=True,
+                                       color="danger",
+                                       id="reset-host",
+                                       style={"text-transform": "none", "width":"100%"})),
+                         dbc.Col(dbc.Button("DELETE HOST",
+                                       outline=True,
+                                       color="danger",
+                                       id="delete-host",
+                                       style={"text-transform": "none", "width":"100%"}))],
+                        style={'margin-bottom': '1rem'}
+                    ),
                     dash_table.DataTable(
                         id='comp-table',
                         columns=[
@@ -178,10 +196,12 @@ RESOURCES_TABLE = dbc.Card(
                             {'name': 'Hostname', 'id': 'hostname'},
                             {'name': 'Total Workers', 'id': 'num_workers'},
                             {'name': 'Total CPU', 'id': 'num_cpus'},
-                            {'name': 'Total GPU', 'id': 'num_gpus'}
+                            {'name': 'Total GPU', 'id': 'num_gpus'},
+                            {'name': 'CPU usage', 'id': 'cpu_usage'},
+                            {'name': 'GPU usage', 'id': 'gpu_usage'}
                         ],
                         data=[],
-                        hidden_columns=['host_id'],
+                        hidden_columns=['host_id', 'cpu_usage', 'gpu_usage'],
                         row_selectable='single',
                         style_cell={'padding': '1rem',
                                     'textAlign': 'left',
@@ -199,11 +219,19 @@ RESOURCES_TABLE = dbc.Card(
 )
 
 
+WARNING_MSG = html.Div([
+    dcc.ConfirmDialog(id="warning-body")
+])
+
+
 MAIN_COLUMN = html.Div([
     RESOURCES_PLOT,
     RESOURCES_TABLE,
-    dcc.Interval(id='interval', interval=5 * 1000, n_intervals=0)]
-)
+    dcc.Interval(id='interval', interval=5 * 1000, n_intervals=0),
+    WARNING_MSG,
+    html.Div(id='no-display',
+             children=[dcc.Store(id='action', data=-1)]),
+])
 
 
 ##### DEFINE LAYOUT ####
@@ -218,50 +246,129 @@ app.layout = html.Div(
             MESSAGE,
             NEW_HOST
         ], fluid=True
-        )
+        ),
+        dcc.Store(id="host-store", data=[])
     ]
 )
 
 ##### CALLBACKS ####
 @app.callback(
-    Output('comp-table', 'data'),
-    Input('query-host', 'n_clicks'),
-    State('query-name', 'value'),
-    State('query-hostname', 'value')
+    Output("comp-table", "data"),
+    Output("host-store", "data"),
+    Input("query-host", "n_clicks"),
+    Input("close-msg", "n_clicks"),
+    Input('interval', 'n_intervals'),
+    State("query-name", "value"),
+    State("query-hostname", "value"),
+    State("host-store", "data")
 )
-def load_resources_list(query_n_clicks, name, hostname):
+def load_resources_list(query_n_clicks, close_msg, time_interval, name, hostname, host_store):
     '''
     This callback dynamically populates the list of computing hosts
     Args:
-        query_n_clicks:     Button that triggers the callback
-        name:               Nickname of compute resource
-        hostname:           Hostname of compute resource
+        query_n_clicks:     [int] Button that triggers the callback
+        close_msg:          [int] Button that closes pop-up messages (when updating/deleting hosts of database)
+        time_interval:      [int] time
+        name:               [str] Nickname of compute resource
+        hostname:           [str] Hostname of compute resource
     Returns:
-        comp_table:         List of compute resources
+        comp_table:         [List] List of compute resources
     '''
     host_list = requests.get(f'{COMP_URL}hosts', params={'hostname': hostname, 'nickname': name}).json()
-    comp_table = []
-    if host_list:
-        for host in host_list:
-            num_workers = host['frontend_constraints']['num_nodes'] + host['backend_constraints']['num_nodes']
-            num_cpus = host['frontend_constraints']['num_processors'] + host['backend_constraints']['num_processors']
-            num_gpus = host['frontend_constraints']['num_gpus'] + host['backend_constraints']['num_gpus']
-            comp_table.append({'host_id': host['uid'],
-                               'nickname': host['nickname'],
-                               'hostname': host['hostname'],
-                               'num_workers': num_workers,
-                               'num_cpus': num_cpus,
-                               'num_gpus': num_gpus})
-    return comp_table
+    if host_store != host_list:
+        comp_table = []
+        host_store = host_list
+        if host_list:
+            for host in host_list:
+                num_workers = host['frontend_constraints']['num_nodes'] + host['backend_constraints']['num_nodes']
+                num_cpus = host['frontend_constraints']['num_processors'] + host['backend_constraints']['num_processors']
+                num_gpus = host['frontend_constraints']['num_gpus'] + host['backend_constraints']['num_gpus']
+                cpu_usage = host['frontend_constraints']['num_processors'] + host['backend_constraints']['num_processors']\
+                            - host['backend_available']['num_processors'] - host['frontend_available']['num_processors']
+                gpu_usage = host['frontend_constraints']['num_gpus'] + host['backend_constraints']['num_gpus'] - \
+                            host['backend_available']['num_gpus'] - host['frontend_available']['num_gpus']
+                comp_table.append({'host_id': host['uid'],
+                                   'nickname': host['nickname'],
+                                   'hostname': host['hostname'],
+                                   'num_workers': num_workers,
+                                   'num_cpus': num_cpus,
+                                   'num_gpus': num_gpus,
+                                   'cpu_usage': cpu_usage,
+                                   'gpu_usage': gpu_usage})
+    else:
+        comp_table = dash.no_update
+        host_store = dash.no_update
+    return comp_table, host_store
 
 
 @app.callback(
-    Output('modal-msg', 'is_open'),
+    Output('resources-plot', 'figure'),
+    Input('comp-table', 'selected_rows'),
+    Input('interval', 'n_intervals'),
+    Input("comp-table", "data"),
+    State('resources-plot', 'figure'),
+    prevent_initial_call=True
+)
+def plot_resources(row, time, comp_table, current_fig):
+    '''
+    This callback plots the current number of resources utilized at the host location
+    Args:
+        row:                    [List] Selected row in list
+        time:                   [int] Time
+        comp_table:             [List] Data in table
+        current_fig:            [fig] Current resources plot
+    Returns:
+        resources_plot:         [fig] Plot of resources
+    '''
+    fig = go.Figure(go.Scatter(x=[], y=[]))
+    fig.update_layout(margin=dict(l=20, r=160, t=20, b=20))
+    changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
+    if row:
+        if row[0]<len(comp_table):
+            host = comp_table[row[0]]
+            host_id = host["host_id"]
+            num_processors = host["cpu_usage"]
+            num_gpus = host["gpu_usage"]
+            resources = {}
+            if len(current_fig['data'][0]['x']) > 0 and 'comp-table.selected_rows' not in changed_id:
+                resources['time'] = np.append(current_fig['data'][0]['x'], current_fig['data'][0]['x'][-1]+1)
+                resources['num_processors'] = np.append(current_fig['data'][0]['y'], num_processors)
+                resources['num_gpus'] = np.append(current_fig['data'][1]['y'], num_gpus)
+            else:
+                resources['time'] = [0]
+                resources['num_processors'] = [num_processors]
+                resources['num_gpus'] = [num_gpus]
+            df = pd.DataFrame.from_dict(resources)
+            df.set_index('time', inplace=True)
+            try:
+                fig = px.line(df, markers=True)
+                fig.update_layout(margin=dict(l=20, r=20, t=20, b=20))
+            except Exception as e:
+                print(e)
+    fig.update_layout(xaxis_title="time[s]", yaxis_title="resources")
+    return fig
+
+
+@app.callback(
+    Output("warning-body", "message"),
+    Output("warning-body", "displayed"),
+    Output("msg-body", "children"),
+    Output("modal-msg", "is_open"),
     Output('new-host', 'is_open'),
-    Output('msg-body', 'children'),
+    Output("action", "data"),
+
+    Input("reset-database", "n_clicks"),
+    Input("reset-host", "n_clicks"),
+    Input("delete-host", "n_clicks"),
+    Input("comp-table", "selected_rows"),
+    Input("warning-body", "cancel_n_clicks"),
+    Input("warning-body", "submit_n_clicks"),
+    Input("close-msg", "n_clicks"),
     Input('open-new-host', 'n_clicks'),
-    Input('close-msg', 'n_clicks'),
     Input({'type':'submit-new-host', "index": ALL}, 'n_clicks'),
+
+    State("comp-table", "data"),
+    State("action", "data"),
     State({'type':'nickname', "index": ALL}, 'value'),
     State({'type':'hostname', "index": ALL}, 'value'),
     State({'type':'privacy', "index": ALL}, 'value'),
@@ -273,37 +380,99 @@ def load_resources_list(query_n_clicks, name, hostname):
     State({'type':'back-list-gpus', "index": ALL}, 'value'),
     prevent_initial_call=True
 )
-def submit_new_host(open_new_host, close_msg, submit_new_host, nickname, hostname, privacy, front_num_workers, front_num_cpus,
-                    front_list_gpus, back_num_workers, back_num_cpus, back_list_gpus):
+def show_messages(reset_database_click, reset_host_click, delete_host_click, row, open_new_host, submit_new_host,
+                  cancel_del_click, confirm_del_click, close_error_click, comp_table, action, nickname, hostname,
+                  privacy, front_num_workers, front_num_cpus, front_list_gpus, back_num_workers, back_num_cpus,
+                  back_list_gpus):
     '''
-    This callback submits a new host to the compute API
+    This callback managers pop-up messages
     Args:
-        open_new_host:          Open form to submit new host
-        close_msg:              Button to close modal with message
-        submit_new_host:        Submit new host
+        reset_database_click:   [int] Button "reset databaset" has been selected
+        reset_host_click:       [int] Button "reset host" has been selected
+        delete_host_click:      [int] Button "delete host" has been selected
+        row:                    [List] Selected row in comp_table
+        open_new_host:          [int]  Open form to submit new host
+        submit_new_host:        [int] Submit new host
+        cancel_del_click:       [int] Button "cancel" has been selected in confirmation window
+        confirm_del_click:      [int] Button "confirm" has been selected in confirmation window
+        close_error_click:      [int] Close message
+        comp_table:             [List] Information in comp_table
+        action:                 [int] Action to confirm
         nickname:               [str] Host nickname
         hostname:               [str] Hostname
+        privacy:                [bool] public or not public
         front_num_workers:      [int] Number of workers for frontend services
         front_num_cpus:         [int] Number of processors for frontend services
         front_list_gpus:        [str] List of GPUs for frontend services
         back_num_workers:       [int] Number of workers for backend services
         back_num_cpus:          [int] Number of processors for backend services
         back_list_gpus:         [str] List of GPUs for backend services
-        privacy:                [bool] public or not public
     Return:
-        open_msg:               Open confirmation message
-        open_host_form:         Open new host form
-        msg_body:               Body of confirmation message
+        warning_body:           [str] Warning message body
+        warning_open:           [bool] Open/close warning message
+        msg_body:               [str] Pop-up messahe body
+        msg_open:               [bool] Open pop-up message
+        open_host_form:         [bool] Open new host form
+        action:                 [int] Action to confirm
     '''
     changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
-    open_msg = dash.no_update
-    open_host_form = dash.no_update
+    warning_body = dash.no_update
+    warning_open = dash.no_update
     msg_body = dash.no_update
-    if 'close-msg.n_clicks' in changed_id:
-        open_msg = False
-    if 'open-new-host.n_clicks' in changed_id:
+    msg_open = dash.no_update
+    open_host_form = dash.no_update
+    
+    if 'reset-database' in changed_id:
+        warning_body ='The workflow database will be deleted with this action. Do you want to continue?'
+        warning_open = True
+        action = 0
+
+    if 'reset-host' in changed_id:
+        if row is None:
+            msg_body = "Select a host from the table"
+            msg_open = True
+        else:
+            nickname = comp_table[row[0]]["nickname"]
+            warning_body = f'The host ({nickname}) resources will be reset. Do you want to continue?'
+            warning_open = True
+            action = 1
+
+    if 'delete-host' in changed_id:
+        if row is None:
+            msg_body = "Select a host from the table"
+            msg_open = True
+            action = -1
+        else:
+            nickname = comp_table[row[0]]["nickname"]
+            warning_body = f'The host ({nickname}) will be deleted with this action. Do you want to continue?'
+            warning_open = True
+            action = 2
+
+    if 'warning-body.cancel_n_clicks' in changed_id:
+        warning_open = False
+        action = -1
+
+    if 'warning-body.submit_n_clicks' in changed_id:
+        if action == 0:
+            response = requests.delete(f'{COMP_URL}system/reset').status_code
+        if action == 1:
+            host_uid = comp_table[row[0]]["host_id"]
+            response = requests.patch(f'{COMP_URL}host/{host_uid}/reset').status_code
+        if action == 2:
+            host_uid = comp_table[row[0]]["host_id"]
+            response = requests.delete(f'{COMP_URL}host/{host_uid}/delete').status_code
+        warning_open = False
+        action = -1
+        if response == 200:
+            msg_body = 'The changes were completed succesfully'
+        else:
+            msg_body = f'Error {response}'
+        msg_open = True
+
+    if 'open-new-host' in changed_id:
         open_host_form = True
-    if '{"index":0,"type":"submit-new-host"}.n_clicks' in changed_id:
+
+    if 'submit-new-host' in changed_id:
         mlex_host = MlexHost(nickname[0], hostname[0], front_num_workers[0], front_num_cpus[0],
                              front_list_gpus[0], back_num_workers[0], back_num_cpus[0],
                              back_list_gpus[0])
@@ -312,57 +481,14 @@ def submit_new_host(open_new_host, close_msg, submit_new_host, nickname, hostnam
             msg_body = 'The host has been sucessfully submitted!'
         else:
             msg_body = f'The host was not submitted. Error code: {response.status_code}'
-        open_msg = True
+        msg_open = True
         open_host_form = False
-    return open_msg, open_host_form, msg_body
 
+    if 'close-msg' in changed_id:
+        msg_open = False
+        action = -1
 
-@app.callback(
-    Output('resources-plot', 'figure'),
-    Input('comp-table', 'selected_rows'),
-    Input('interval', 'n_intervals'),
-    State('resources-plot', 'figure'),
-    State('comp-table', 'data'),
-)
-def plot_resources(row, time, current_fig, comp_table):
-    '''
-    This callback plots the current number of resources utilized at the host location
-    Args:
-        row:                    Selected row in list
-        time:                   Time
-        current_fig:            Current resources plot
-        comp_table:             Data in table
-    Returns:
-        resources_plot:         Plot of resources
-    '''
-    fig = go.Figure(go.Scatter(x=[], y=[]))
-    fig.update_layout(margin=dict(l=20, r=160, t=20, b=20))
-    changed_id = [p['prop_id'] for p in dash.callback_context.triggered][0]
-    if row:
-        host_id = comp_table[row[0]]["host_id"]
-        host = requests.get(f'{COMP_URL}hosts/{host_id}').json()
-        num_processors = host['frontend_constraints']['num_processors'] + host['backend_constraints']['num_processors'] - \
-                         host['backend_available']['num_processors'] - host['frontend_available']['num_processors']
-        num_gpus = host['frontend_constraints']['num_gpus'] + host['backend_constraints']['num_gpus'] - \
-                   host['backend_available']['num_gpus'] - host['frontend_available']['num_gpus']
-        resources = {}
-        if len(current_fig['data'][0]['x']) > 0 and 'comp-table.selected_rows' not in changed_id:
-            resources['time'] = np.append(current_fig['data'][0]['x'], current_fig['data'][0]['x'][-1]+1)
-            resources['num_processors'] = np.append(current_fig['data'][0]['y'], num_processors)
-            resources['num_gpus'] = np.append(current_fig['data'][1]['y'], num_gpus)
-        else:
-            resources['time'] = [0]
-            resources['num_processors'] = [num_processors]
-            resources['num_gpus'] = [num_gpus]
-        df = pd.DataFrame.from_dict(resources)
-        df.set_index('time', inplace=True)
-        try:
-            fig = px.line(df, markers=True)
-            fig.update_layout(margin=dict(l=20, r=20, t=20, b=20))
-        except Exception as e:
-            print(e)
-    fig.update_layout(xaxis_title="time[s]", yaxis_title="resources")
-    return fig
+    return warning_body, warning_open, msg_body, msg_open, open_host_form, action
 
 
 if __name__ == "__main__":
